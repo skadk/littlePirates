f8fee62fce7c0b9eb4ae9271f2f6a698e3e92f5b
dbswjd241 <yoonjung241@naver.com>
2023-01-18 17:33:04 +0900

    kidscafe 자세히보기 관련 고치기

--- littlePirates/src/main/java/com/littlePirates/project/controller/KidscafeController.java
+++ littlePirates/src/main/java/com/littlePirates/project/controller/KidscafeController.java
@@ -24,6 +24,7 @@ public class KidscafeController {
 		return "kidscafe/kidscafe_map";
 	}
 
+	// 키즈카페 지도(이름, 위도, 경도 )불러오기
 	@ResponseBody
 	@RequestMapping("/kidscafe_map/loaddata")
 	public ArrayList<KidscafeVO> loaddata() {
@@ -35,7 +36,9 @@ public class KidscafeController {
 	@RequestMapping("/kidscafe_sec")
 	public String kidscafe_sec(Model model) {
 		ArrayList<KidscafeVO> voList = service.listKidscafeInfo();
+		ArrayList<KidscafeVO> voList2 = service.listKidscafeInfo2();
 		model.addAttribute("voList", voList);
+		model.addAttribute("voList2", voList2);
 		
 		return "kidscafe/kidscafe_sec";
 	}
