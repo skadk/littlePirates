04784a6dd31ea154224ace6a8925e600870745f2
daehosong <thdeogh136@gmail.com>
2023-01-26 01:43:48 +0900

    체크리스트 뷰페이지 CSS , 생활습관 체크리스트 CSS , 달성률 페이지 일부 제작

--- littlePirates/src/main/java/com/littlePirates/project/dao/IChecklistDAO.java
+++ littlePirates/src/main/java/com/littlePirates/project/dao/IChecklistDAO.java
@@ -1,6 +1,7 @@
 package com.littlePirates.project.dao;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import com.littlePirates.project.model.ChecklistVO;
 
@@ -11,4 +12,10 @@ public interface IChecklistDAO {
 	public ArrayList<ChecklistVO> checklistInfo2();
 	
 	public ArrayList<ChecklistVO> checklistInfo3();
+
+	public void insertChecklist(ChecklistVO vo);
+
+	public void updateChecklist(ChecklistVO vo);
+
+	public int ischhChecked(HashMap<String, Object> map);
 }
--- littlePirates/src/main/java/com/littlePirates/project/service/IChecklistService.java
+++ littlePirates/src/main/java/com/littlePirates/project/service/IChecklistService.java
@@ -1,5 +1,6 @@
 package com.littlePirates.project.service;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
 
 import com.littlePirates.project.model.ChecklistVO;
@@ -8,4 +9,6 @@ public interface IChecklistService {
 	public ArrayList<ChecklistVO> checklistInfo();
 	public ArrayList<ChecklistVO> checklistInfo2();
 	public ArrayList<ChecklistVO> checklistInfo3();
+	void insertChecklist(ChecklistVO vo);
+	int ischhChecked(Timestamp chhDate, String memId);
 }
