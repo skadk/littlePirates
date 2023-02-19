ef55714de90a71e44f98f0f352e111737de50014
skadk <120160786+skadk@users.noreply.github.com>
2023-02-19 21:49:58 +0900

    페이징 완성

--- littlePirates/src/main/java/com/littlePirates/project/dao/IChildrenDAO.java
+++ littlePirates/src/main/java/com/littlePirates/project/dao/IChildrenDAO.java
@@ -4,10 +4,8 @@ import java.util.ArrayList;
 
 import com.littlePirates.project.model.ChildrenVO;
 import com.littlePirates.project.model.KindergartenVO;
-import com.littlePirates.project.model.PagingVO;
 
 public interface IChildrenDAO {
-	public ArrayList<ChildrenVO> NurseryList(PagingVO vo);
 	public ArrayList<KindergartenVO> KindergartenList();
 	public int countBoard();
 
--- littlePirates/src/main/java/com/littlePirates/project/service/IChildrenService.java
+++ littlePirates/src/main/java/com/littlePirates/project/service/IChildrenService.java
@@ -3,12 +3,9 @@ package com.littlePirates.project.service;
 import java.util.ArrayList;
 
 
-import com.littlePirates.project.model.ChildrenVO;
 import com.littlePirates.project.model.KindergartenVO;
-import com.littlePirates.project.model.PagingVO;
 
 public interface IChildrenService {
-	public ArrayList<ChildrenVO> NurseryList(PagingVO vo);
 	public ArrayList<KindergartenVO> KindergartenList();
 	public int countBoard();
 	
